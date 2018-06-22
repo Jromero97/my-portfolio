eb8cf2682c6b96251866b92612f93b6884ea47d7
JR Romero <jrromero@192.168.1.9>
2018-06-22 03:42:53 -0600

    Fixing Home Component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {Switch, Route} from 'react-router';
-import AboutMe from '../../components/AboutMe';
-import ContactMe from '../../components/ContactMe';
+import {Switch, Route} from 'react-router-dom';
+// import AboutMe from '../../components/AboutMe';
+// import ContactMe from '../../components/ContactMe';
 import Home from '../../components/Home';
 
 
 const Main = () => (
     <main>
         <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/about-me' component={AboutMe}/>
-            <Route path='/contact-me' component={ContactMe}/>
+            <Route exact path='/' render={() => <Home/>}/>
         </Switch>
     </main>
 );
